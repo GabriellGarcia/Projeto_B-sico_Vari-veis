999d73ee979b1a37263b317020daa20f7847e1e6
[EXT] - GABRIEL GARCIA MORAIS <gamorais@brasilseg.com.br>
2024-09-05 12:40:25 -0300

    Projeto Básico

---
+++ Projeto Básico.py
@@ -0,0 +1,21 @@
+# Criando Variáveis
+Nome = 'Gabriel'
+Idade = '20'
+Esporte = 'Futebol'
+Fruta = 'Manga'
+Jogo = 'Valorant'
+Melhor_do_Mundo = 'Neymar'
+Plataforma = 'YouTube'
+Números = '85K'
+Visualizações = '10 Milhões'
+Canal = 'Drankao'
+Animal = 'Tigrinho'
+
+print('Salve Galera!')
+print('Sou ' + Nome + ', Tenho ' + Idade + ' anos e trabalho na área de produção do T.i com os crias!')
+print('Às Quintas vamos no ' + Fruta + ' ou no Caíres tomar uma de Levs.')
+print('Sou bonito, jogo bola, GTA RP, ' + Jogo + ', e LoL, e claro, o pai dança.')
+print('Me comparam muito com o ' + Melhor_do_Mundo + ' em tudo, rs.')
+print('Também tenho um canal no ' + Plataforma + ' que conta com ' + Números + ' inscritos e com ' + Visualizações + ' de visualizações.')
+print('Caso queira saber mais, procure por ' + Canal + ' :D')
+print ('Mas se tudo der errado vou para o ' + Animal + '.')
